86656b5c0567a540549fe235a3811e3c5d8832f0
Picman1 <joop@flexicloud.co.za>
2025-06-20 07:05:54 +0200

    Delete src/Secrets.h

--- src/Secrets.h
+++
@@ -1,7 +0,0 @@
-const char* ssid = "LionVirus";
-const char* password = "*****";
-
-const char* mqtt_user = "jr";      // optional
-const char* mqtt_password = "****";  // optional
-
-const char* otaPassword = "****";  // OTA authentication password
