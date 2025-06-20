b43dac8e14c1c292800eef68b51ef43cbadc46aa
Picman1 <joop@flexicloud.co.za>
2025-06-20 07:06:11 +0200

    WIP: save current changes before rebase

--- src/Secrets.h
+++ src/Secrets.h
@@ -1,7 +1,7 @@
 const char* ssid = "LionVirus";
-const char* password = "*****";
+const char* password = "DieHereIsMyHerder";
 
 const char* mqtt_user = "jr";      // optional
-const char* mqtt_password = "****";  // optional
+const char* mqtt_password = "Groenpunt1";  // optional
 
-const char* otaPassword = "****";  // OTA authentication password
+const char* otaPassword = "Groenpunt1";  // OTA authentication password
