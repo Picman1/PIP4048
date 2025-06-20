71ad39f04a1631bbb00b645258958b6a95c41afb
Picman1 <joop@flexicloud.co.za>
2025-06-20 06:30:00 +0200

    Update Secrets.h

--- src/Secrets.h
+++ src/Secrets.h
@@ -2,6 +2,6 @@ const char* ssid = "LionVirus";
 const char* password = "*****";
 
 const char* mqtt_user = "jr";      // optional
-const char* mqtt_password = "Groenpunt1";  // optional
+const char* mqtt_password = "****";  // optional
 
-const char* otaPassword = "Groenpunt1";  // OTA authentication password
+const char* otaPassword = "****";  // OTA authentication password
